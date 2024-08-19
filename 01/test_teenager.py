aed402e3d58eeb006bc5b8559183d8a5a7865fd5
Broum11 <165827478+Broum11@users.noreply.github.com>
2024-08-19 14:23:15 +0200

    Create test_teenager.py

---
+++ 01/test_teenager.py
@@ -0,0 +1,7 @@
+vek = input('Zadej svůj věk: ')
+vek = int(vek) # z inputu vyleze string musí se předělat na int(číslo)
+if vek > 12 and vek < 20 : 
+    print("Jsi teenager") 
+else:
+    print("Nejsi teenager")
+    
