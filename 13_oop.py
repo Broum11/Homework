fcab3ad7a77b5ec705d7182183dfba99937d2713
Broum11 <165827478+Broum11@users.noreply.github.com>
2024-09-17 20:20:45 +0200

    Create 13_oop.py

---
+++ 13_oop.py
@@ -0,0 +1,25 @@
+'''
+3. vytvořte soubor 13_oop.py
+a v něm třídu Planeta, která bude obsahovat:
+atributy:
+- poradi 
+- nazev
+a metodu info
+která řekne "Toto je [název planety] a je [pořadí] od Slunce"
+vytvořte 2 instance
+- Země
+- a nějakou další planetu
+'''
+class Planeta:
+    def __init__(self,nazev,poradi):
+        self.nazev = nazev
+        self.poradi = poradi
+
+    def info(self): # metoda
+        print(f"Toto je {self.nazev} a je {self.poradi} od Slunce")     
+
+Zeme = Planeta ('Země', 3)
+Merkur = Planeta ('Merkur', 1)
+
+Zeme.info() # metoda se musí zavolat
+Merkur.info()
